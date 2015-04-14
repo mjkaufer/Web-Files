1c0077c8b49a0f8675df6ca96ed31c875173948c
Matthew Kaufer <mjkaufer@gmail.com>
2015-04-14 15:07:36 -0400

    Better form, new PHP

--- labs/Lab6.php
+++ labs/Lab6.php
@@ -1,16 +1,49 @@
 <?php 
 
-    $maxLength = $_GET["maxLength"];
-    $firstLetter = $_GET["firstLetter"];
+	$letters = array();
 
-    $remainingLength = intval($maxLength) - 1;
-    
-    // echo $remainingLength . "\n";
+	//(?=.*$letter{num,})
+
+	for ($i = 1; $i <= 8; $i++) {
+    	$letter = $_GET[$i . ""];
+    	if($letter != "*"){//not a wild card
+    		$letters[] = $letter;
+    	}
+	}
+
+	$map = array_count_values($letters);
+	$uniques = count($map);
+	$regex = "";
+
+	foreach (map as $letter => $freq){
+		$regex += "(?=.*" . $letter . "{" . $freq . "})";
+	}
+
+	$regex += "\w{" . $uniques . ",}";
+
+	//todo - right now, it finds only words with the letters specified
+	//make it better
+
+    $pattern = '/\b' . $regex . '\s/'; 
 
     $file = file_get_contents("dict.txt"); 
-
     
-    $pattern = '/\b' . $firstLetter . '\w{0,'. $remainingLength . '}\s/'; 
+    print_r("DEBUG<br>" . PHP_EOL);
+
+    print_r("<br>");
+
+    print_r($regex);
+
+    print_r("<br>");
+
+	print_r("<br>");
+
+    print_r($pattern);
+
+    print_r("<br>");
+
+    print_r("\\DEBUG<br>" . PHP_EOL);
+
     // echo $maxLength . "|" . $firstLetter . "|" . $remainingLength . "|" . $pattern;
     preg_match_all($pattern, $file, $matches ); 
 
