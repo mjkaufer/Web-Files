5aa4ce5ee48c3475d76442b31f8c3ef205894cc6
Matthew Kaufer <mjkaufer@gmail.com>
2015-04-14 15:28:29 -0400

    Fixed Scrabble PHP

--- labs/Lab6.php
+++ labs/Lab6.php
@@ -1,25 +1,34 @@
 <?php 
 
+	echo "<h1>Scrabble Word Finder Results</h1>";
+
 	$letters = array();
 
 	//(?=.*$letter{num,})
 
 	for ($i = 1; $i <= 8; $i++) {
     	$letter = $_GET[$i . ""];
+
     	if($letter != "*"){//not a wild card
     		$letters[] = $letter;
     	}
 	}
 
 	$map = array_count_values($letters);
+
 	$uniques = count($map);
 	$regex = "";
 
+	// foreach ($map as $key => $value){
+	// 	$regex += "(?=.*" . $key . "{" . $value . "})";
+	// }
+
 	foreach ($map as $letter => $freq){
-		$regex += "(?=.*" . $letter . "{" . $freq . "})";
+		$regex .= "(?=.*" . $letter . "{" . $freq . "})";
 	}
 
-	$regex += "\w{" . $uniques . ",}";
+
+	
 
 	//todo - right now, it finds only words with the letters specified
 	//make it better
@@ -28,26 +37,23 @@
 
     $file = file_get_contents("dict.txt"); 
     
-    print_r("DEBUG<br>" . PHP_EOL);
 
-    print_r("<br>");
+	for ($len = 2; $len <= 12; $len++) {
 
-    print_r($regex);
+		$newRegex = $regex . "\w{" . $len . "}";
+		$pattern = '/\b' . $newRegex . '\s/'; 
 
-    print_r("<br>");
+		preg_match_all($pattern, $file, $matches ); 
 
-	print_r("<br>");
+		if(count($matches[0]) > 0){
+			echo "<h2>" . $len . " Letters Long</h2><br>";
 
-    print_r($pattern);
+			foreach($matches[0] as $word) {
+				echo $word . "\r\n";
+			}
 
-    print_r("<br>");
+			echo "<br>";
+		}
 
-    print_r("\\DEBUG<br>" . PHP_EOL);
-
-    // echo $maxLength . "|" . $firstLetter . "|" . $remainingLength . "|" . $pattern;
-    preg_match_all($pattern, $file, $matches ); 
-
-	foreach($matches[0] as $word) {
-		echo $word . "\r\n";
 	}
 ?>
